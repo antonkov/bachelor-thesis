bbab78ecaf6f343d312a4dc2741d93fcdd881a28
Anton Kovsharov <antonkovsharov@gmail.com>
2015-05-18 19:37:09 +0300

    chapter 2 finished

---
+++ paper/chapters/auto/chapter1.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "chapter1"
+ (lambda ()
+   (LaTeX-add-labels
+    "chapSVD"
+    "strat:comb"
+    "strat:box"
+    "strat:tacks"
+    "strat:subfigures"
+    "tacks-lh")))
+
